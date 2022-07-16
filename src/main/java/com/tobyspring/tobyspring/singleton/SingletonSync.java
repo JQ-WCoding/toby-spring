fb9877483cd9dd62cdf0b8448e71da2cd08827f4
Jun_Q <junlee36520620@gmail.com>
2022-07-16 23:48:13 +0900

    comment 수정

--- src/main/java/com/tobyspring/tobyspring/singleton/SingletonSync.java
+++ src/main/java/com/tobyspring/tobyspring/singleton/SingletonSync.java
@@ -6,11 +6,11 @@ public class SingletonSync {
     private SingletonSync() {
     }
 
+    // synchronized 멀티 쓰레드 환경에서 호출에 대한 예방법(?)
     public static synchronized SingletonSync getInstance() {
         if ( instance == null ) {
             instance = new SingletonSync();
         }
-
         return instance;
     }
 }
