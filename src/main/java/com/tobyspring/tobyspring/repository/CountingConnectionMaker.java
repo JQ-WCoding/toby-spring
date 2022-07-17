dd1f9c490f096036e67721bf780f54075db0ee77
Jun_Q <junlee36520620@gmail.com>
2022-07-17 23:45:35 +0900

    counting class 확인용

---
+++ src/main/java/com/tobyspring/tobyspring/repository/CountingConnectionMaker.java
@@ -0,0 +1,24 @@
+package com.tobyspring.tobyspring.repository;
+
+import java.sql.Connection;
+
+public class CountingConnectionMaker implements ConnectionMaker {
+
+    int counter = 0;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeConnection() {
+        this.counter++;
+
+        return realConnectionMaker.makeConnection();
+    }
+
+    public int getCounter() {
+        return this.counter;
+    }
+}
